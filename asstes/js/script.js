4dc040ac385b795ef753dbe32e74fc7503ec94d7
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-13 15:47:27 +0200

    create list
    add possibilty to add things to do list. add check and trash button

--- asstes/js/script.js
+++ asstes/js/script.js
@@ -1,9 +1,63 @@
 /* Descrizione: Rifare l’esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 text, una stringa che indica il testo del todo
 done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-MILESTONE 1 Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-MILESTONE 2 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 1 Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.☑️
+MILESTONE 2 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.☑️
 MILESTONE 3 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus: 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
 
-console.log('ciao');
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+
+        newTodo:'',
+        error: false,
+        
+        toDo: [
+
+//       example of collect data    
+             {
+                text:'ciao',
+                done: false,  
+            }
+        ]
+
+
+    }
+
+  },
+
+  //function
+  methods: {
+    
+        addList () {
+            
+
+            //verifico che non sia vuoto il campo 
+            if (this.newTodo !== ''){
+
+                //resetto l'errore
+                this.error = false;
+
+                //pusho nell'array il testo e il done
+                this.toDo.push({text: this.newTodo, done: false});
+            } else {
+
+                //mostro l'errore
+                this.error = true;
+            }
+            console.log(this.toDo);
+        },
+
+        removeList(array, i) {
+            this.toDo.splice(this.toDo[i],1);
+        }
+
+
+    }, 
+
+}
+).mount('#app');
